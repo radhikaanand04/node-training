390851d7308cfceb6d34db243eb04570e74bda27
Radhika <radhikajenitha04@gmail.com>
2025-03-18 18:31:34 +0530

    first commit

---
+++ models/products.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const productSchema = mongoose.Schema(
+    {
+        name : {
+            type : String,
+            required : [true,"Please enter the product name:"] 
+        },
+        quantity : {
+            type : Number,
+            required : true,
+            default : 0
+        },
+        price : {
+            type : Number,
+            required : true,
+            default : 0
+        }
+    },
+    {
+    timestamps : true
+}
+)
+
+const Product = mongoose.model("Products",productSchema);
+module.exports = Product;
